40ddfdb2380ded0e5c05167ebeb943ae6515f685
RusTantas <165479418+RusTantas@users.noreply.github.com>
2024-05-27 21:35:49 +0300

    Update HomeWork_2.py

--- HomeWork_2.py
+++ HomeWork_2.py
@@ -1,4 +1,5 @@
 
+
 class InvalidDataException(Exception):
     def __init__(self, massage):
         self.massage = massage
@@ -10,19 +11,27 @@ class ProcessingException(Exception):
 
 
 def generate_exception (a , b):
-    if a == 2:
+    if a == 1:
         raise InvalidDataException('вы ввели вообще не то')
 
-    if b == 1:
+    if b == 0:
         raise ProcessingException('вы не понимаете что надо делать')
 
+def cal_exception ():
+    try:
+        print(generate_exception(a=0, b=0))
+    except InvalidDataException as exept_1:
+        print(f"Сообщение об ошибки {exept_1.massage}")
+    except ProcessingException as exept_2:
+        print(f"Сообщение об ошибки {exept_2.massage}")
 
-try:
-    print(generate_exception(a=1, b=1))
-except InvalidDataException as exept_1:
-    print(f"Сообщение об ошибки {exept_1.massage}")
-except ProcessingException as exept_2:
-    print(f"Сообщение об ошибки {exept_2.massage}")
+cal_exception()
+
+
+
+
+
+
 
 
 
